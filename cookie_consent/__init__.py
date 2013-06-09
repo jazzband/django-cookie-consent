cffce1d77b42503e083e7accf3051a2d47732ca5
Bojan Mihelac <bmihelac@mihelac.org>
2013-06-09 10:15:10 +0200

    Bumb dev version and use simpler versions.

--- cookie_consent/__init__.py
+++ cookie_consent/__init__.py
@@ -1,24 +1,2 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-"""
-cookie_consent
-"""
-__version_info__ = {
-    'major': 0,
-    'minor': 1,
-    'micro': 0,
-    'releaselevel': 'final',
-    'serial': 1
-}
-
-def get_version(short=False):
-    assert __version_info__['releaselevel'] in ('alpha', 'beta', 'final')
-    vers = ["%(major)i.%(minor)i" % __version_info__, ]
-    if __version_info__['micro']:
-        vers.append(".%(micro)i" % __version_info__)
-    if __version_info__['releaselevel'] != 'final' and not short:
-        vers.append('%s%i' % (
-            __version_info__['releaselevel'][0], __version_info__['serial']))
-    return ''.join(vers)
-
-__version__ = get_version()
+VERSION = (0, 1, '2dev')
+__version__ = '.'.join(map(str, VERSION))
