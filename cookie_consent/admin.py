cac13062199553a92cf092c6c7ea884e99ea9001
Bojan Mihelac <bmihelac@mihelac.org>
2013-06-11 16:52:25 +0200

    tweak admin

--- cookie_consent/admin.py
+++ cookie_consent/admin.py
@@ -10,11 +10,29 @@ from .models import (
 )
 
 
+class CookieAdmin(admin.ModelAdmin):
+    list_display = ('varname', 'name', 'cookiegroup', 'path', 'domain',
+                    'get_version')
+    search_fields = ('name', 'domain', 'cookiegroup__varname',
+                     'cookiegroup__name')
+    readonly_fields = ('varname',)
+    list_filter = ('cookiegroup',)
+
+
+class CookieGroupAdmin(admin.ModelAdmin):
+    list_display = ('varname', 'name', 'is_required', 'is_deletable',
+                    'get_version')
+    search_fields = ('varname', 'name',)
+    list_filter = ('is_required', 'is_deletable',)
+
+
 class LogItemAdmin(admin.ModelAdmin):
     list_display = ('action', 'cookiegroup', 'version', 'created')
     list_filter = ('action', 'cookiegroup')
+    readonly_fields = ('action', 'cookiegroup', 'version', 'created')
+    date_hierarchy = 'created'
 
 
+admin.site.register(Cookie, CookieAdmin)
+admin.site.register(CookieGroup, CookieGroupAdmin)
 admin.site.register(LogItem, LogItemAdmin)
-admin.site.register(Cookie)
-admin.site.register(CookieGroup)
