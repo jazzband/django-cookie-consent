5caa921bcbe5ff82a329f3d714b812d215b63edd
Fernando Cordeiro <fernando.cordeiro@jeronimo-martins.com>
2020-03-27 10:38:08 +0000

    ✅ TEST: Add redirect test

--- tests/core/tests/test_views.py
+++ tests/core/tests/test_views.py
@@ -18,6 +18,21 @@ from cookie_consent.models import (
 )
 
 
+class CookieGroupBaseProcessViewTests(TestCase):
+
+    def test_get_success_url(self):
+        """
+        If user adds a 'next' as URL parameter it should,
+        redirect to the value of 'next'
+        """
+        expected_url = reverse('test_page')
+        url = "{}?next={}".format(
+            reverse('cookie_consent_accept_all'), expected_url
+        )
+        response = self.client.post(url, follow=True)
+        self.assertRedirects(response, expected_url)
+
+
 class IntegrationTest(TestCase):
 
     def setUp(self):
