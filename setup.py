c5e78021ca887182129dcf2ec9a682558943bc88
Bojan Mihelac <bmihelac@mihelac.org>
2020-06-15 18:15:32 +0200

    Update classifiers

--- setup.py
+++ setup.py
@@ -6,10 +6,20 @@ version = '0.2.3.dev0'
 
 CLASSIFIERS = [
     'Framework :: Django',
+    'Framework :: Django :: 1.9',
+    'Framework :: Django :: 2.1',
+    'Framework :: Django :: 2.2',
+    'Framework :: Django :: 3.0',    
     'Intended Audience :: Developers',
     'License :: OSI Approved :: BSD License',
     'Operating System :: OS Independent',
     'Topic :: Software Development',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 2',
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
 ]
 
 install_requires = [
