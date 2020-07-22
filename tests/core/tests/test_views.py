b555833ef7e07b6e2b6ce86aeaf7d727cd123c67
Fernando <fernando_cordeiro@live.com>
2020-07-22 12:22:04 +0100

    📦 NEW: Conditional logging option (#29)

--- tests/core/tests/test_views.py
+++ tests/core/tests/test_views.py
@@ -8,6 +8,7 @@ except ImportError:
 from django.test import (
     TestCase,
 )
+from django.test.utils import override_settings
 
 from cookie_consent.models import (
     Cookie,
@@ -131,3 +132,25 @@ class IntegrationTest(TestCase):
             version=self.cookie_group.get_version(),
             action=ACTION_DECLINED)
         self.assertEqual(log_items.count(), 1)
+
+    @override_settings(COOKIE_CONSENT_LOG_ENABLED=False)
+    def test_logging_disabled(self):
+        """
+        When the COOKIE_CONSENT_LOG_ENABLED is False, no log item should be 
+        created
+        """
+        self.client.post(reverse('cookie_consent_accept',
+                                 kwargs={"varname": "optional"}))
+        log_items = LogItem.objects.filter(
+            cookiegroup=self.cookie_group,
+            version=self.cookie_group.get_version(),
+            action=ACTION_ACCEPTED)
+        self.assertEqual(log_items.count(), 0)
+
+        self.client.delete(reverse('cookie_consent_decline',
+                                   kwargs={"varname": "optional"}))
+        log_items = LogItem.objects.filter(
+            cookiegroup=self.cookie_group,
+            version=self.cookie_group.get_version(),
+            action=ACTION_DECLINED)
+        self.assertEqual(log_items.count(), 0)
