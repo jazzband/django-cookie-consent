b840530d098b87dd465ebb69bc93c618f7cf03b4
Fernando Cordeiro <fernando_cordeiro@live.com>
2021-12-08 10:10:35 +0000

    Remove Python 2,  Django <2.2 support

--- tests/core/tests/test_views.py
+++ tests/core/tests/test_views.py
@@ -1,14 +1,9 @@
 # -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
-try:
-    from django.urls import reverse
-except ImportError:
-    from django.core.urlresolvers import reverse
 from django.test import (
     TestCase,
 )
 from django.test.utils import override_settings
+from django.urls import reverse
 
 from cookie_consent.models import (
     Cookie,
