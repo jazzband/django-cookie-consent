346fcc660e65a27514d6776ca7350c96b66de299
Sergei Maertens <sergei@maykinmedia.nl>
2022-09-24 16:28:15 +0200

    :recycle: [#13] -- refactor clean cookies middleware implementation
    This should make it easier to understand how it works
    and what it's supposed to be doing.
    
    Note: the case where cookie version is compared with group version
    is removed. It's unclear what the logic behind this is, and it's
    probably also wrong because the group version is derived from the
    first item cookie in the cookie group, but this has no explicit
    ordering and may be based on database ID or timestamp, so
    it's at least unreliable on different database backends.

--- cookie_consent/middleware.py
+++ cookie_consent/middleware.py
@@ -1,7 +1,27 @@
 # -*- coding: utf-8 -*-
-from cookie_consent.cache import all_cookie_groups
-from cookie_consent.conf import settings
-from cookie_consent.util import get_cookie_dict_from_request, is_cookie_consent_enabled
+from typing import Optional
+
+from .cache import all_cookie_groups
+from .conf import settings
+from .util import get_cookie_dict_from_request, is_cookie_consent_enabled
+
+
+def _should_delete_cookie(group_version: Optional[str]) -> bool:
+    # declined after it was accepted (and set) before
+    if group_version == settings.COOKIE_CONSENT_DECLINE:
+        return True
+
+    # if you need to opt-out instead of opt-in, then we only delete the cookie in the
+    # above scenario -> when the group is explicitly declined
+    if settings.COOKIE_CONSENT_OPT_OUT:
+        return False
+
+    # when we are opt-in and have no information whether the cookie group was accepted
+    # or declined, delete the cookie(s).
+    if group_version is None:
+        return True
+
+    return False
 
 
 class CleanCookiesMiddleware:
@@ -16,28 +36,26 @@ class CleanCookiesMiddleware:
 
     def __call__(self, request):
         response = self.get_response(request)
-        self.process_response(request, response)
+        if is_cookie_consent_enabled(request):
+            self.process_response(request, response)
         return response
 
     def process_response(self, request, response):
-        if not is_cookie_consent_enabled(request):
-            return response
         cookie_dic = get_cookie_dict_from_request(request)
+
+        cookies_to_delete = []
         for cookie_group in all_cookie_groups().values():
             if not cookie_group.is_deletable:
                 continue
+
             group_version = cookie_dic.get(cookie_group.varname, None)
             for cookie in cookie_group.cookie_set.all():
-                if cookie.name not in request.COOKIES.keys():
+                if cookie.name not in request.COOKIES:
                     continue
-                if group_version is None and not settings.COOKIE_CONSENT_OPT_OUT:
-                    response.delete_cookie(cookie.name, cookie.path, cookie.domain)
-                    continue
-                if group_version == settings.COOKIE_CONSENT_DECLINE:
-                    response.delete_cookie(cookie.name, cookie.path, cookie.domain)
-                if (
-                    group_version < cookie.get_version()
-                    and not settings.COOKIE_CONSENT_OPT_OUT
-                ):
-                    response.delete_cookie(cookie.name, cookie.path, cookie.domain)
+                if _should_delete_cookie(group_version):
+                    cookies_to_delete.append(cookie)
+
+        for cookie in cookies_to_delete:
+            response.delete_cookie(cookie.name, cookie.path, cookie.domain)
+
         return response
